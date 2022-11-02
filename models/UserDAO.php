9eeb19ad0079a0078710f1687069a0ee48a0bba0
Nabile Sall <idrissa.sall@etu.univ-lyon1.fr>
2022-11-02 21:51:09 +0100

    Création du dossier models et ajout des classe connexion, DAO et userDAO pour la connexion à la base de donnée. Ajout des constantes pour la connexion à la base de donnée dans le fichier debug.php

--- models/UserDAO.php
+++ models/UserDAO.php
@@ -4,7 +4,7 @@ require_once(PATH_MODELS.'DAO.php');
 class UserDAO extends DAO{
     
     /**
-     * Retourne l'enregistrement d'un user à partir de son userName
+     * Retourne l'enregistrement d'un user à partir de son userName 
      */
     public function getUser(String $userName){
         $requete = "SELECT * FROM User where UserName = '$userName'";
