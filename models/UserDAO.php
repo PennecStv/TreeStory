b5d5d261556dc2d541261e1b25692810faa4637c
SALL IDRISSA p2111661 <idrissa.sall@etu.univ-lyon1.fr>
2022-11-06 18:29:48 +0000

    Création du dossier models et ajout des classe connexion, DAO et userDAO pour...

---
+++ models/UserDAO.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Models;
+
+require_once(PATH_UTILS_DATABASE.'DAO.php');
+
+/**
+ * It extends from class DAO. It defines methods for 
+ * retrieving specific data from the database
+ * 
+ * @author Idrissa Sall <idrissa.sall@etu.univ-lyon1.fr>
+ */
+class UserDAO extends DAO{
+    
+    /**
+     * We access the data of a user knowing his userName
+     * 
+     * @return false|PDOStatement        query results
+     */
+    public function getUser(String $userName){
+        $requete = "SELECT * FROM User where UserName = '$userName'";
+        return $this->queryAll($this->$requete);
+    }
+}
+
+?>
