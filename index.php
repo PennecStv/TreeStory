d3fcc3a925ca6761ce8ac437ade92cc5d9c657c1
MONTMAIN JONATHAN p2100659 <jonathan.montmain@etu.univ-lyon1.fr>
2023-01-16 16:14:44 +0000

    Ajout de toutes les fonctionnalités d'histoire et corrections de bugs

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
  * @author  Idrissa Sall        <idrissa.sall@etu.univ-lyon1.fr>
  */
 
+session_save_path("/tmp");
 session_start();
 
 require_once("./vendor/autoload.php");
@@ -27,7 +28,8 @@ require_once(PATH_CONTROLLERS . "UserController.php");
 require_once(PATH_CONTROLLERS . "PasswordController.php");
 require_once(PATH_CONTROLLERS . "RegisterController.php");
 require_once(PATH_CONTROLLERS . "ConfigProfilController.php");
-require_once(PATH_CONTROLLERS . "accountController.php");
+require_once(PATH_CONTROLLERS . "AccountController.php");
+require_once(PATH_CONTROLLERS . "StoryController.php");
 
 use Symfony\Component\Dotenv\Dotenv;
 use Routing\Router;
@@ -45,11 +47,12 @@ $router->route(Method::GET, "/login", ["UserController", "login"]);
 $router->route(Method::POST, "/login", ["UserController", "login"]);
 $router->route(Method::GET, "/logout", ["UserController", "logout"]);
 
-$router->route(Method::GET, "/forgotPassword", ["PasswordController", "forgotPassword"]);
-$router->route(Method::POST, "/forgotPassword", ["PasswordController", "forgotPassword"]);
+$router->route(Method::GET, "/forgot-password", ["PasswordController", "forgotPassword"]);
+$router->route(Method::POST, "/forgot-password", ["PasswordController", "forgotPassword"]);
+$router->route(Method::GET, "/reset-password", ["PasswordController", "resetPassword"]);
+$router->route(Method::POST, "/reset-password", ["PasswordController", "resetPassword"]);
 
-$router->route(Method::GET, "/resetPassword", ["PasswordController", "resetPassword"]);
-$router->route(Method::POST, "/resetPassword", ["PasswordController", "resetPassword"]);
+$router->route(Method::GET, "/story/:id/show", ["StoryController", "get"]);
 
 $router->route(Method::GET, "/register", ["RegisterController", "register"]);
 $router->route(Method::POST, "/register", ["RegisterController", "register"]);
@@ -57,6 +60,39 @@ $router->route(Method::POST, "/register", ["RegisterController", "register"]);
 $router->route(Method::GET, "/configProfil", ["ConfigProfilController", "configProfil"]);
 $router->route(Method::POST, "/configProfil", ["ConfigProfilController", "configProfil"]);
 
+$router->route(Method::GET, "/account", ["AccountController", "account"]);
+$router->route(Method::GET, "/story/create", ["StoryController", "get_creation_form"]);
+$router->route(Method::POST, "/story/create", ["StoryController", "create"]);
+
+$router->route(Method::GET, "/register", ["RegisterController", "register"]);
+$router->route(Method::POST, "/register", ["RegisterController", "register"]);
+
+$router->route(Method::GET, "/story/chapter/:id/read", ["StoryController", "read"]);
+
+$router->route(Method::GET, "/story/chapter/:id/edit", ["StoryController", "get_node_edition_form"]);
+$router->route(Method::POST, "/story/chapter/:id/edit", ["StoryController", "edit"]);
+
+$router->route(Method::GET, "/story/chapter/:id/contribute", ["StoryController", "get_node_creation_form"]);
+$router->route(Method::POST, "/story/chapter/:id/contribute", ["StoryController", "create_node"]);
+$router->route(Method::GET, "/user/account", ["AccountController", "account"]);
+$router->route(Method::GET, "/user/:userId", ["AccountController", "displayAccount"]);
+$router->route(Method::POST, "/user/:id/follow", ["AccountController", "follow"]);
+$router->route(Method::POST, "/user/:id/unfollow", ["AccountController", "unfollow"]);
+
+$router->route(Method::GET, "/account", ["AccountController", "account"]);
+$router->route(Method::GET, "/story/create", ["StoryController", "get_creation_form"]);
+$router->route(Method::POST, "/story/create", ["StoryController", "create"]);
+
+$router->route(Method::GET, "/register", ["RegisterController", "register"]);
+$router->route(Method::POST, "/register", ["RegisterController", "register"]);
+
+$router->route(Method::GET, "/story/chapter/:id/read", ["StoryController", "read"]);
+
+$router->route(Method::GET, "/story/chapter/:id/edit", ["StoryController", "get_node_edition_form"]);
+$router->route(Method::POST, "/story/chapter/:id/edit", ["StoryController", "edit"]);
+
+$router->route(Method::GET, "/story/chapter/:id/contribute", ["StoryController", "get_node_creation_form"]);
+$router->route(Method::POST, "/story/chapter/:id/contribute", ["StoryController", "create_node"]);
 $router->route(Method::GET, "/user/account", ["AccountController", "account"]);
 $router->route(Method::GET, "/user/:userId", ["AccountController", "displayAccount"]);
 $router->route(Method::POST, "/user/:id/follow", ["AccountController", "follow"]);
@@ -64,6 +100,7 @@ $router->route(Method::POST, "/user/:id/unfollow", ["AccountController", "unfoll
 
 
 $router->route(Method::ERROR, "404", ["ErrorController", "notFound"]);
+$router->route(Method::ERROR, "500", ["ErrorController", "internal"]);
 
 $router->run();
 
